514558f06ba9dfa15f2cb278c7d8419afe0eb19c
Eli <13420359+eliasm307@users.noreply.github.com>
2023-05-10 21:51:54 +0100

    setup test structure

--- src/myContextMenuCommand.ts
+++ src/myContextMenuCommand.ts
@@ -1,9 +1,22 @@
+// managing tree items: https://code.visualstudio.com/api/extension-guides/tree-view
+
 import * as vscode from "vscode";
+import fs from "fs";
+
+function fileExists(filePath: string): boolean {
+  try {
+    fs.accessSync(filePath, fs.constants.F_OK);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
 
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     "extension.myContextMenuCommand",
-    (uri: vscode.Uri) => {
+    (uri: vscode.Uri, ...args) => {
+      console.log("myContextMenuCommand", uri, args);
       vscode.window.showInformationMessage(`Context Menu Option Clicked: ${uri.fsPath}`);
     },
   );
