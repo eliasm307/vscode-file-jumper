c52fcf4174defbef4bb3bddf64c082bb53cbd0a4
Eli <13420359+eliasm307@users.noreply.github.com>
2023-05-12 02:26:04 +0100

    with full coverage for FileType

--- src/classes/FileType.test.ts
+++ src/classes/FileType.test.ts
@@ -2,21 +2,21 @@ import { describe, it, assert } from "vitest";
 import FileType from "./FileType";
 
 describe.only("FileType", () => {
-  describe("#getRelatedFile", () => {
-    function createFileTypeWithRegisteredFiles(): FileType {
-      const fileType = new FileType({
-        name: "test",
-        marker: "🧪",
-        regex: "\\/test\\/(.*)\\.test\\.ts",
-      });
-      fileType.registerPaths([
-        "/test/relatedFile0.test.ts",
-        "/test/dir1/relatedFile1.test.ts",
-        "/test/dir1/dir2/relatedFile2.test.ts",
-      ]);
-      return fileType;
-    }
+  function createFileTypeWithRegisteredFiles(): FileType {
+    const fileType = new FileType({
+      name: "test",
+      marker: "🧪",
+      regex: "\\/test\\/(.*)\\.test\\.ts",
+    });
+    fileType.registerPaths([
+      "/test/relatedFile0.test.ts",
+      "/test/dir1/relatedFile1.test.ts",
+      "/test/dir1/dir2/relatedFile2.test.ts",
+    ]);
+    return fileType;
+  }
 
+  describe("#getRelatedFile", () => {
     it("should return the related file", () => {
       const fileType = createFileTypeWithRegisteredFiles();
 
@@ -45,6 +45,44 @@ describe.only("FileType", () => {
       const fileType = createFileTypeWithRegisteredFiles();
       const relatedFile = fileType.getRelatedFile("dir1/otherFile.ts");
       assert.isUndefined(relatedFile, "related file should not be found");
+    });
+  });
+
+  describe("#matches", () => {
+    it("should return true if the file is related", () => {
+      const fileType = createFileTypeWithRegisteredFiles();
+      assert.isTrue(fileType.matches("/test/dir1/relatedFile1.test.ts"), "file should match");
+    });
+
+    it("should return false if the file is not related", () => {
+      const fileType = createFileTypeWithRegisteredFiles();
+      assert.isFalse(fileType.matches("/test/dir1/otherFile.ts"), "file should not match");
+    });
+  });
+
+  describe("#reset", () => {
+    it("should clear the registered files", () => {
+      const fileType = createFileTypeWithRegisteredFiles();
+      assert.deepStrictEqual(
+        fileType.getRelatedFile("dir1/relatedFile1"),
+        {
+          typeName: "test",
+          marker: "🧪",
+          fullPath: "/test/dir1/relatedFile1.test.ts",
+        },
+        "related file should be found",
+      );
+
+      assert.isTrue(fileType.matches("/test/dir1/relatedFile1.test.ts"), "file should match");
+
+      fileType.reset();
+
+      assert.isUndefined(
+        fileType.getRelatedFile("dir1/relatedFile1"),
+        "related file should not be found",
+      );
+
+      assert.isFalse(fileType.matches("/test/dir1/relatedFile1.test.ts"), "file should not match");
     });
   });
 });
--- src/classes/FileType.ts
+++ src/classes/FileType.ts
@@ -6,33 +6,21 @@ export type RelatedFileData = {
   fullPath: string;
 };
 
-/**
- * @key the file path between the path prefix and suffix from patterns or the full path of a known related file
- * @value Details of found files that match the key path for this group
- */
-type PatternGroupFiles = Record<string, RelatedFileData[]>;
-
 export default class FileType {
-  private keyPathToFullPathMap: Record<string, string> = {};
+  private keyPathToFullPathMap: Map<string, string> = new Map();
 
   private regex: RegExp;
 
-  constructor(
-    public readonly config: FileTypeConfig,
-    private readonly options?: {
-      // todo when does this get fired
-      onFileRelationshipChange: (filePath: string) => void;
-    },
-  ) {
+  constructor(public readonly config: FileTypeConfig) {
     this.regex = new RegExp(config.regex);
   }
 
   matches(filePath: string): boolean {
-    throw new Error("Method not implemented.");
+    return this.regex.test(filePath);
   }
 
   getRelatedFile(keyPath: string): RelatedFileData | undefined {
-    const fullPath = this.keyPathToFullPathMap[keyPath];
+    const fullPath = this.keyPathToFullPathMap.get(keyPath);
     if (!fullPath) {
       return;
     }
@@ -48,85 +36,16 @@ export default class FileType {
     filePaths.forEach((fullPath) => {
       const keyPath = this.getKeyPath(fullPath);
       if (keyPath) {
-        this.keyPathToFullPathMap[keyPath] = fullPath;
+        this.keyPathToFullPathMap.set(keyPath, fullPath);
       }
     });
   }
 
   reset() {
-    this.keyPathToFullPathMap = {};
+    this.keyPathToFullPathMap.clear();
   }
 
   private getKeyPath(filePath: string): string | undefined {
-    // todo this implicitly creates a regex for each call, we should create one before and reuse it
     return filePath.match(this.regex)?.[1];
   }
-
-  /*
-  addFile(newFilePath: string) {
-    // todo update file candidates and also update context for context menu conditional showing
-    // see https://code.visualstudio.com/api/references/when-clause-contexts#in-and-not-in-conditional-operators
-
-    this.config.fileGroupConfigs.forEach((fileGroupConfig, patternGroupIndex) => {
-      this.fileGroupsByPatternGroupIndexMap[patternGroupIndex] ??= {};
-      const patternGroupFiles = this.fileGroupsByPatternGroupIndexMap[patternGroupIndex];
-
-      fileGroupConfig.types.forEach((filetTypeConfig) => {
-        const keyPath = this.getKeyPath(newFilePath, filetTypeConfig);
-        if (!keyPath) {
-          return;
-        }
-
-        const newFileData: RelatedFileData = {
-          name: filetTypeConfig.name,
-          marker: filetTypeConfig.marker,
-          fullPath: newFilePath,
-          shortPath: getShortPath(newFilePath),
-        };
-
-        patternGroupFiles[keyPath] ??= [];
-        patternGroupFiles[keyPath].push(newFileData);
-
-        patternGroupFiles[newFilePath] ??= [];
-        patternGroupFiles[newFilePath].push(newFileData);
-      });
-    });
-  }
-
-  getRelatedFileGroups(currentFilePath: string): RelatedFileData[][] {
-    const groupedRelatedFiles: RelatedFileData[][] = [];
-
-    this.config.fileGroupConfigs.forEach((patternGroup, patternGroupIndex) => {
-      const groupFiles = this.fileGroupsByPatternGroupIndexMap[patternGroupIndex];
-      const groupOutput: RelatedFileData[] = [];
-
-      patternGroup.types.forEach((fileTypeConfig) => {
-        // try the cheap lookup first
-        let relatedFilesForPattern = groupFiles[currentFilePath];
-
-        // if that fails, try the slightly more expensive lookup by calculating the key path
-        if (!relatedFilesForPattern) {
-          const keyPath = this.getKeyPath(currentFilePath, fileTypeConfig);
-          if (keyPath) {
-            relatedFilesForPattern = groupFiles[keyPath];
-          }
-
-          // if we still dont have any related files, skip this pattern
-          if (!relatedFilesForPattern) {
-            return;
-          }
-        }
-
-        groupOutput.push(...relatedFilesForPattern);
-      });
-
-      if (groupOutput.length) {
-        groupedRelatedFiles.push(groupOutput);
-      }
-    });
-
-    console.log("getRelatedFileGroups", { currentFilePath, groupedRelatedFiles });
-    return groupedRelatedFiles;
-  }
-  */
 }
